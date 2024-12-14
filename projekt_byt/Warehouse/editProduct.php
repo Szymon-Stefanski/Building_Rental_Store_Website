85d0b9c701d7f046bf38cfbab9be42989f80a79f
s26838-pj <115404399+s26838-pj@users.noreply.github.com>
2024-12-14 23:48:00 +0100

    Update editProduct.php

--- projekt_byt/Warehouse/editProduct.php
+++ projekt_byt/Warehouse/editProduct.php
@@ -132,7 +132,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edytuj Produkt</title>
-    <link rel="stylesheet" href="../Style/style_add.editProduct.css">
+    <link rel="stylesheet" href="../Style/style_editProduct.css">
  </head>
  <body>
      <header class="header">
